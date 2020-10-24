34a277e22eee9d4280fb996f822e04a3d140f252
Lucas Amoncio <lucasamoncio@gmail.com>
2020-10-24 18:07:50 +0800

    Attempted to make triangle formula

--- geometry.rb
+++ geometry.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
 print "Please enter side A: "
-side_a = gets
-puts side_a + 1
+side_a = gets.chomp()
+print "Please enter side B: "
+side_b = gets.chomp()
+print "Please enter side C: "
+side_c = gets.chomp()
+
+puts S = (side_a + side_b + side_b) / 2.to_i
+
+puts sqrt(S * (S - side_a.to_i) * (S - side_b.to_i) * (S - side_c.to_i))
+
