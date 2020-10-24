419dd1c7989a5a81a51033905a3a347f4fa883b2
Lucas Amoncio <lucasamoncio@gmail.com>
2020-10-24 19:05:56 +0800

    Debugged the program

--- geometry.rb
+++ geometry.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/env ruby
 
 print "Please enter side A: "
-side_a = gets.chomp()
+side_a = gets.to_f
 print "Please enter side B: "
-side_b = gets.chomp()
+side_b = gets.to_f
 print "Please enter side C: "
-side_c = gets.chomp()
+side_c = gets.to_f
 
-puts S = (side_a + side_b + side_b) / 2.to_i
+s = (side_a + side_b + side_c) / 2
+area = Math.sqrt(s * (s - side_a) * (s - side_b) * (s - side_c))
 
-puts sqrt(S * (S - side_a.to_i) * (S - side_b.to_i) * (S - side_c.to_i))
+puts format("The area of the triangle is %.2f.", area)
 
