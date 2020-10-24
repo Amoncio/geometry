34533b7c608c27d98bcb1bf7539ce80b893ee8af
Jem Pimentel <jem.pimentel@outlook.com>
2020-10-24 19:59:24 +0800

    Added exception handling

--- geometry.rb
+++ geometry.rb
@@ -7,15 +7,16 @@ side_b = gets.to_f
 print "Please enter side C: "
 side_c = gets.to_f
 
+begin
+  raise Math::DomainError unless side_a.positive? && side_b.positive? && side_c.positive?
 
-s = (side_a + side_b + side_c) / 2
-heron = s * (s - side_a) * (s - side_b) * (s - side_c)
-if heron.positive?
+  s = (side_a + side_b + side_c) / 2
+  heron = s * (s - side_a) * (s - side_b) * (s - side_c)
+
+  raise Math::DomainError unless heron.positive?
+
   area = Math.sqrt(heron)
   puts format("The area of the triangle is %.2f.", area)
-else
+rescue Math::DomainError
   puts "That ain't a triangle, buddy."
 end
-
-
-
